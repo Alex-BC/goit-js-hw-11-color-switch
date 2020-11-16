9e2a3a3a89d5b7113396cfa6f1c6b0844160309d
Alexandr Chepak <djalexbc@gmail.com>
2020-11-16 10:29:31 +0200

    some edits

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
-import {colors} from "./colors.js"
+import { colors } from './colors.js';
 
 const refs = {
   startBtn: document.querySelector('[data-action="start"]'),
   stopBtn: document.querySelector('[data-action="stop"]'),
-  body: document.querySelector('body')
-}
+  body: document.querySelector('body'),
+};
 // console.log(refs.body);
 // console.log(refs.startBtn);
 
-refs.startBtn.addEventListener('click', () => { changeBackgroundColor.start() })
-refs.stopBtn.addEventListener('click', () => { changeBackgroundColor.stop() })
+refs.startBtn.addEventListener('click', () => {
+  changeBackgroundColor.start();
+});
+refs.stopBtn.addEventListener('click', () => {
+  changeBackgroundColor.stop();
+});
 
-
-console.log(colors[0])
+console.log(colors[0]);
 
 const changeBackgroundColor = {
   getNewColorId: null,
   isActive: false,
-  
 
   start() {
+    if (this.isActive) {
+      return;
+    }
 
-    if(this.isActive) {
-    return
-    }   
-    
-    refs.body.style.backgroundColor = '#ffffff'
+    refs.body.style.backgroundColor = '#ffffff';
     this.isActive = true;
     this.getNewColorId = setInterval(() => {
-      
-      refs.body.style.backgroundColor = `${colors[randomIntegerFromInterval(0, 5)]}`
-     
-      
-        }, 1000);
-},
+      refs.body.style.backgroundColor = `${
+        colors[randomIntegerFromInterval(0, 5)]
+      }`;
+    }, 1000);
+  },
 
-    stop() {
-      clearInterval(this.getNewColorId)
-      this.isActive = false
-    }
-}
+  stop() {
+    clearInterval(this.getNewColorId);
+    this.isActive = false;
+  },
+};
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
-
 };
